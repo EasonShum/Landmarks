db8f5dbc184eaf883e52c3cbb5d54d8939119a30
EasonShum <55642618+EasonShum@users.noreply.github.com>
2020-04-13 20:30:28 +0800

    Add files via upload

---
+++ WorkingWithUIControls/Landmarks/Landmarks/Profile/ProfileHost.swift
@@ -0,0 +1,27 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by cycu on 2020/4/13.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State var draftProfile = Profile.default
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
